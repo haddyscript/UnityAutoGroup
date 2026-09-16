5f069113717ebaea92f2c58b608a7921b20272cb
hadrianfdc <162952181+hadrianfdc@users.noreply.github.com>
2026-09-16 13:46:05 +0800

    Add Lenis smooth scroll

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,5 @@
+import 'lenis/dist/lenis.css'
+import { ReactLenis } from 'lenis/react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
@@ -6,8 +8,10 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ReactLenis root options={{ lerp: 0.1, duration: 1.2, smoothWheel: true }}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ReactLenis>
   </StrictMode>,
 )
