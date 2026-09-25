0f0283fa853743a7119885a0cb96a70f9bd09257
hadrianfdc <162952181+hadrianfdc@users.noreply.github.com>
2026-09-25 09:32:48 +0800

    Remove redundant Shop/Mobile Service sections from home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,10 +3,8 @@ import { FAQSection } from '../components/home/FAQSection'
 import { FinalCTA } from '../components/home/FinalCTA'
 import { Hero } from '../components/home/Hero'
 import { HowItWorks } from '../components/home/HowItWorks'
-import { MobileServiceSection } from '../components/home/MobileServiceSection'
 import { QuoteEntry } from '../components/home/QuoteEntry'
 import { ServicesOverview } from '../components/home/ServicesOverview'
-import { ShopServiceSection } from '../components/home/ShopServiceSection'
 import { SymptomChecker } from '../components/home/SymptomChecker'
 import { TestimonialsSection } from '../components/home/TestimonialsSection'
 import { WhyChooseUs } from '../components/home/WhyChooseUs'
@@ -20,8 +18,6 @@ export default function Home() {
       <SymptomChecker />
       <WhyChooseUs />
       <HowItWorks />
-      <ShopServiceSection />
-      <MobileServiceSection />
       <AboutSection />
       <TestimonialsSection />
       <FAQSection />
